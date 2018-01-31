c2205d4b45fe2b587642444e0238bdd4ad938b99
tallyb <tally.barak@gmail.com>
2018-01-31 14:25:24 +0200

    testobject location

--- features/steps.js
+++ features/steps.js
@@ -8,4 +8,18 @@ defineSupportCode(function ({
     Given(/^Do Something$/, function () {
         browser.waitForEnabled('//article/a[.="Improve this doc"]');
     });
+
+    Given(
+        /^User is located in latitude: (\-?\d+\.?\d*), longitude: (\-?\d+\.?\d*)$/,
+        function setUserLocation(latitude, longitude) {    
+            let location = { latitude: parseFloat(latitude), longitude: parseFloat(longitude), altitude: 0 };
+            browser.setGeoLocation(location);   
+            if (!browser.isIOS && !browser.isAndroid) {
+                browser.url('/');    
+                //browser.location(location);
+            } else {
+                
+            }
+        }
+    );
 });
